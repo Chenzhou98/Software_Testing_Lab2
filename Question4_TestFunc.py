b37df0c80079785038764a5a2795da937cbc68f6
Chenzhou <171994636@qq.com>
2023-02-15 16:53:45 +0100

    pairwise test

--- Question4_TestFunc.py
+++ Question4_TestFunc.py
@@ -16,41 +16,7 @@ def RandomArrayGenerator(length=6, low=0, high=100):
     return array
 
 
-import copy
-import itertools
-from sys import stdout
-from loguru import logger
-def parewise(option):
-    cp = []  #original combination
-    s = []  #seperate
-    for x in eval('itertools.product' + str(tuple(option))):
-        cp.append(x)
-        print(cp)
-        s.append([i for i in itertools.combinations(x, 2)])
-    logger.info('original combination' % len(cp))
-    del_row = []
-    s2 = copy.deepcopy(s)
-    for i in range(len(s)):
-        t = 0
-        for j in range(len(s[i])):
-            flag = False
-            for i2 in [x for x in range(len(s2)) if s2[x] != s[i]]:
-                if s[i][j] == s2[i2][j]:
-                    t = t + 1
-                    flag = True
-                    break
-            if not flag:
-                break
-        if t == len(s[i]):
-            del_row.append(i)
-            s2.remove(s[i])
-    res = [cp[i] for i in range(len(cp)) if i not in del_row]
-    logger.info('pairwise combination:%s' % len(res))
-    return res
-
-
-
-def Test():
+def Test_Random():
     counter = 1
     errorfound = 0
     while errorfound != 1:
@@ -63,20 +29,75 @@ def Test():
         if correct_result == mutated_result:
             counter = counter + 1
         else:
-            print("Testing method:",)
+            print("Testing method: Random Testing",)
             print("Correct result is ", correct_result, "whereas mutated result is ", mutated_result)
             print("Mutaion error is found in loop ", counter)
             errorfound = 1
     return
 
+def Test_Pairwise():
+    counter = 1
+    errorfound = 0
+    input = pairwise() # generate pairwise array
+    print(input)
+    while errorfound != 1:
+        for elements in input: # choose one combination from pairwise array
+            key = elements[random.randint(0, len(elements)-1)]
+            #print(key, elements)
+            correct_result = Membership_unsorted(elements, key)
+            mutated_result = Mutation_2.Membership_unsorted(elements, key)  # to be modified
+            #print (input, key, correct_result, mutated_result)
+            if correct_result == mutated_result:
+                counter = counter + 1
+            else:
+                print("Testing method: Pairwise Testing",)
+                print("Correct result is ", correct_result, "whereas mutated result is ", mutated_result)
+                print("Mutaion error is found in loop ", counter)
+                errorfound = 1
+                break
+    return
+
+def pairwise(length=6, low= 1, high=100):
+    defaults = []
+    typicals = []
+    testCases = []
+    """
+    # generate M=length: M dimension. n=2: pairwise test case
+    # For example: a length=3 dimension pairwise testing
+    # Typical1 = { Sat, Sun }
+    # Typical2 = { A, B }
+    # Typical3 = { 1, 2 }
+    # have 
+    # defaults = ["Sat","A","1"]
+    # typicals = ["Sun","B","2"]
+    # which have testcase
+    # (def1, def2, def3), [0-wise]
+    # (Sun, def2, def3), (def1, B, def3), (def1, def2, 2), [1-wise]
+    # (Sun, B, def3), (Sun, def2, 2), (def1, B, 2) [2-wise]
+    """
+    # Separate typical and default into 2 arrays
+    while len(defaults) < length:
+        defaults.append(random.randint(low, high))
+        typicals.append(random.randint(low, high))
+    #print(defaults, typicals)
+    #defaults = ["Sat","A","1"]
+    #typicals = ["Sun","B","2"]
+
+    #0-wise
+    testCases.append(defaults)
+    #1-wise
+    for element in range(length):
+        newTestCase = defaults[0:element] + typicals[element:element + 1] + defaults[element + 1:]
+        testCases.append(newTestCase)
+    #2-wise
+    for element1 in range(length):
+        for element2 in range(element1 + 1, length):
+            newTestCase = defaults[0:element1] + typicals[element1:element1 + 1] + \
+                          defaults[element1 + 1:element2] + typicals[element2:element2 + 1] \
+                          + defaults[element2 + 1:]
+            testCases.append(newTestCase)
+    return testCases # testcase include 0, 1, 2 wise
+
 if __name__ == "__main__":
 
-    Test()
-
-    """
-    pl = [['M', 'O', 'P'], ['W', 'L', 'I'], ['C', 'E',"O"]]
-    a = parewise(pl)
-    print()
-    for i in a:
-        print(i)
-    """
+    Test_Pairwise()
